b26e013471d3680f253960cd735096fd54d1bee1
lilmingwa13 <minh.phamquang1302@gmail.com>
2024-04-06 16:56:51 +0700

    Update forntend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './layouts/header-footer/Navbar';
+import Footer from './layouts/header-footer/Footer';
+import HomePage from './layouts/homepage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './layouts/about/About';
+import ChiTietSanPham from "./layouts/product/ChiTietSanPham";
+import DangKyNguoiDung from "./layouts/user/DangKyNguoiDung";
+import DangNhap from './layouts/user/DangNhap';
+import SachForm from './layouts/admin/SachForm';
 
-function App() {
+ 
+function App(): React.JSX.Element {
+  const [tuKhoaTimKiem, setTuKhoaTimKiem] = useState('');
+
   return (
-    <div className="App">
-      BOOKSTORE PROJECT
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar tuKhoaTimKiem={tuKhoaTimKiem}  setTuKhoaTimKiem={setTuKhoaTimKiem}/>
+        <Routes>
+             <Route path='/' element={<HomePage tuKhoaTimKiem={tuKhoaTimKiem} />} />
+             <Route path='/:maTheLoai' element={<HomePage tuKhoaTimKiem={tuKhoaTimKiem} />} />
+             <Route path='/about' element={<About />} />
+             <Route path='/sach/:maSach' element={<ChiTietSanPham />} />
+             <Route path='/dang-ky' element={<DangKyNguoiDung />} />
+             <Route path='/dang-nhap' element={<DangNhap />} />
+             <Route path='/admin/them-sach' element={<SachForm />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
